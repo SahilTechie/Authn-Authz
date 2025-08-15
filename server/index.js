83a12f56ed5e0876e53ea34ddbc33b45b20b9f96
SahilTechie <smallelw@gitam.in>
2025-08-16 00:05:46 +0530

    Fix port binding and start script for Render deployment

--- server/index.js
+++ server/index.js
@@ -53,6 +53,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 }); 
